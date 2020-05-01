fa5ec21b97fec1b75c969a153a6f5412e46addc6
harshithkrpt <harshith.krpt@gmail.com>
2020-05-01 17:19:06 +0530

    Typescript Added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-require("dotenv/config");
-const { GraphQLClient } = require("graphql-request");
+import "dotenv/config";
+import { GraphQLClient } from "graphql-request";
 
 (async () => {
   const token = process.env.GITHUB_GRAPHQL_API;
 
   if (token) {
-    const endPoint = "https://api.github.com/graphql";
+    const endPoint: string = "https://api.github.com/graphql";
     const graphQLClient = new GraphQLClient(endPoint, {
       headers: {
         authorization: `Bearer ${token}`,
